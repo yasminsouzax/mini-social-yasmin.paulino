aabaedff7b1626ee349748697fe6a8a697319edf
yasminsouzax <yasmin.9273322@aluno.mg.gov.br>
2026-04-08 13:52:37 +0000

    alterar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let likeCount = 0;
 let curtido = false; // flag booleana
 
-function curtido() {
+function curtir() {
   if(curtido == false){
   likeCount++;
   curtido = true;
@@ -18,7 +18,7 @@ document.getElementById("likeBtn").addEventListener("click", curtir);
 let deslikeCount = 0;
 let deslike = false;
 
-function deslike() {
+function descurtir() {
   if(deslike == false){
   deslikeCount++;
   deslike = true;
