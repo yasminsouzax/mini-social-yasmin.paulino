0a436975f077eb18fc453e7de8f62fc61c5c587d
yasminsouzax <yasmin.9273322@aluno.mg.gov.br>
2026-05-06 16:00:21 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,48 +1,65 @@
-//=== ESTADO (dados da aplicação) === 
+//===banco de dados (json simulado) === 
 
-let likeCount = 0;
-let dislikeCount = 0;
-let curtido = false;
-let descurtido = false;
+let post = {
+  likeCount: 0,
+  dislikeCount: 0,
+  curtido: false,
+  descurtido: false
+}
 
 //=== SERVICE (regras de negócio) === 
 
 function curtir() {
-  if (curtido == false){
-    likeCount++;
-    curtido = true;
+  if (post.curtido == false){
+    post.likeCount++;
+    post.curtido = true;
 
-    if(descurtido == true){
-      dislikeCount--;
-      descurtido = false;
+    if(post.descurtido == true){
+      post.dislikeCount--;
+      post.descurtido = false;
     }
 
   }else{
-    likeCount--;
-    curtido = false;
+    post.likeCount--;
+    post.curtido = false;
   }
 
 }
 
 function descurtir() {
-  if(descurtido == false){
-    dislikeCount++;
-    descurtido = true;
+  if(post.descurtido == false){
+    post.dislikeCount++;
+    post.descurtido = true;
 
-    if(curtido == true){
-      likeCount--;
-      curtido = false;
+    if(post.curtido == true){
+      post.likeCount--;
+      post.curtido = false;
     }
 
   }
   else{
-    dislikeCount--;
-    descurtido = false;
+    post.dislikeCount--;
+    post.descurtido = false;
   }
 }
 
+// API simulada ==
+function getPost(){
+  return post;
+}
+
+function likePost(){
+  curtir();
+  return post;
+}
+
+function dislikePost(){
+  descurtir();
+  return post;
+}
+
 // === VIEW (interface/renderização)===
-function atualizarTela(){
+function atualizarTela(){ 
   document.getElementById("likeCount").innerText = likeCount;
   document.getElementById("dislikeCount").innerText = dislikeCount;
 }
